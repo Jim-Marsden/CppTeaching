37a01698bf5a8dbb51b026d7135e039a5f80ad40
james <jamespmarsden@hotmail.com>
2021-09-23 13:37:36 -0700

    Initial commit

---
+++ lesson_4_functions/lesson2_main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by james on 9/23/2021.
+//
+// Hello, this lesson will be on values, and variables
+
+int main(){
+
+
+    auto my_number = 0;
+    /**
+     * Alright, lets look at that line. auto means automatic type deduction, and there's going to be more on types in
+     * the next lesson, so we can skip that.
+     *
+     * my_number is a variable. Think of them as a container for values. Typically, you can change the stored value, but
+     * not always.
+     *
+     * and values are the raw data, like "strings", 'c'hars, and numbers. They're just plain old data. You cannot assign
+     * to a value, only variables.
+     */
+
+    my_number = 1;
+
+    return my_number;
+}
+
+
